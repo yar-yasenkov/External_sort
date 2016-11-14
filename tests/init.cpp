574e020b07a5316e6875aefd7e67a9d6ad903708
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-11-14 16:18:24 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,8 +1,9 @@
 
 #include <catch.hpp>
+#include "external_sort.h"
 using namespace std;
 SCENARIO("init", "[INIT]"){
-
+        Simple_Merging_Sort("spisok.txt");
 	bool mark=true;
 
 	REQUIRE(mark);
