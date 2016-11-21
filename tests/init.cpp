9efe7089bd5fee3fd9136b43671059cf4d67b7b1
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-11-21 09:55:38 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,14 +4,14 @@
 #include "natural_merge_sort.h"
 using namespace std;
 SCENARIO("init_merge_sort", "[INIT]"){
-        Simple_Merging_Sort("spisok.txt");
+       // Simple_Merging_Sort("spisok.txt");
 	bool mark=true;
 
 	REQUIRE(mark);
 }
 
 SCENARIO("init_natural_merge_sort", "[INIT2]"){
-       // Natural_Merging_Sort("spisok.txt");
+        Natural_Merging_Sort("spisok.txt");
 	bool mark=true;
 	REQUIRE(mark);
 }
