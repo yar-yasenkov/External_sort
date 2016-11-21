fad9c57e745a67b829e100ea8871dcdde8a115a0
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-11-21 09:40:49 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,6 +1,7 @@
 
 #include <catch.hpp>
 #include "external_sort.h"
+#include "natural_merge_sort.h"
 using namespace std;
 SCENARIO("init_merge_sort", "[INIT]"){
         Simple_Merging_Sort("spisok.txt");
