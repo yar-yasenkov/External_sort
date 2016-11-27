f289351a3dd64c1a8392aa633f10786969d0b12c
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-11-27 19:41:51 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -3,15 +3,28 @@
 #include "external_sort.h"
 #include "natural_merge_sort.h"
 using namespace std;
-SCENARIO("init_merge_sort", "[INIT]"){
-        Simple_Merging_Sort("spisok.txt", "out.txt", 512);
-	bool mark=true;
+//SCENARIO("init_merge_sort", "[INIT]"){
+//        Simple_Merging_Sort("spisok.txt", "out.txt", 512);
+//	bool mark=true;
 
+//	REQUIRE(mark);
+//}
+
+SCENARIO("init_natural_merge_sort", "[INIT1]"){
+        Natural_Merging_Sort("8.txt", "out.txt", 512);
+	bool mark=true;
 	REQUIRE(mark);
 }
 
+
 SCENARIO("init_natural_merge_sort", "[INIT2]"){
-        Natural_Merging_Sort("spisok.txt", "out.txt", 512);
+        Natural_Merging_Sort("15.txt", "out.txt", 512);
+	bool mark=true;
+	REQUIRE(mark);
+}
+
+SCENARIO("init_natural_merge_sort", "[INIT3]"){
+        Natural_Merging_Sort("32.txt", "out.txt", 512);
 	bool mark=true;
 	REQUIRE(mark);
 }
