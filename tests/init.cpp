e02cd48f82b4bd49fc716ded00b9d66b9cac161e
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-11-27 20:25:41 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -12,28 +12,17 @@ SCENARIO("init_sort1", "[INIT1]")
         bool mark=true;
 	REQUIRE(mark);
 }
-//SCENARIO("init_merge_sort", "[INIT]"){
-//        Simple_Merging_Sort("spisok.txt", "out.txt", 512);
-//	bool mark=true;
 
-//	REQUIRE(mark);
-//}
-
-//SCENARIO("init_natural_merge_sort", "[INIT1]"){
-//        Natural_Merging_Sort("8.txt", "out.txt", 512);
-//	bool mark=true;
-//	REQUIRE(mark);
-//}
-
-/*
-SCENARIO("init_natural_merge_sort", "[INIT2]"){
-        Natural_Merging_Sort("15.txt", "out.txt", 512);
+SCENARIO ("init_sort2", "[INIT2]")
+{
+	sortlines("15.txt", "out.txt", 4);
 	bool mark=true;
 	REQUIRE(mark);
 }
 
-SCENARIO("init_natural_merge_sort", "[INIT3]"){
-        Natural_Merging_Sort("32.txt", "out.txt", 512);
+CENARIO ("init_sort3", "[INIT3]")
+{
+	sortlines("32.txt", "out.txt", 17);
 	bool mark=true;
 	REQUIRE(mark);
-}*/
+}
