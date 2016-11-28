c23b251a42016c4140c418ba28780fee79f16fbd
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-11-28 14:56:13 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -10,6 +10,17 @@ SCENARIO("init_sort1", "[INIT1]")
 {
 	sortlines("8.txt", "out.txt", 1);
         bool mark=true;
+	line s1,s2;
+	ifstream input_sorted("sorted8.txt"); 
+	ifstream input_result("out.txt");
+	
+	while (!input_result.eof())
+	{
+		input_sorted >> s1;
+		input_result >> s2;
+		if (s1.name != s2.name)
+		  mark=false;
+	}	
 	REQUIRE(mark);
 }
 
