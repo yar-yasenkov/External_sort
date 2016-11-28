e5a43f96bc59d4616291980f7ab8acd01f04dd4c
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-11-28 16:13:58 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -8,8 +8,13 @@ using namespace std;
 
 SCENARIO("init_sort1", "[INIT1]")
 {
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+        start = std::chrono::system_clock::now();
 	sortlines("8.txt", "out.txt", 1);
-        bool mark=true;
+        end = std::chrono::system_clock::now(); 
+	int elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(end-start).count();
+	std::cout << elapsed_seconds << std::endl;
+	bool mark=true;
 	line s1,s2;
 	ifstream input_sorted("sorted8.txt"); 
 	ifstream input_result("out.txt");
@@ -26,8 +31,13 @@ SCENARIO("init_sort1", "[INIT1]")
 
 SCENARIO ("init_sort2", "[INIT2]")
 {
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+        start = std::chrono::system_clock::now();
 	sortlines("15.txt", "out.txt", 1);
-        bool mark=true;
+        end = std::chrono::system_clock::now(); 
+	int elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(end-start).count();
+	std::cout << elapsed_seconds << std::endl;
+	bool mark=true;
 	line s1,s2;
 	ifstream input_sorted("sorted15.txt"); 
 	ifstream input_result("out.txt");
@@ -44,8 +54,13 @@ SCENARIO ("init_sort2", "[INIT2]")
 
 SCENARIO ("init_sort3", "[INIT3]")
 {
+
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+        start = std::chrono::system_clock::now();
 	sortlines("32.txt", "out.txt", 1);
-        bool mark=true;
+        int elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(end-start).count();
+	std::cout << elapsed_seconds << std::endl;
+	bool mark=true;
 	line s1,s2;
 	ifstream input_sorted("sorted32.txt"); 
 	ifstream input_result("out.txt");
